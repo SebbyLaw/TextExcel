c203555dd4aeb8fe9c23539f401e8124a445801a
SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>
2020-03-27 19:08:17 -0700

    javadocs for BaseCell
    Signed-off-by: SebbyLaw <44045823+SebbyLaw@users.noreply.github.com>

--- src/textExcel/BaseCell.java
+++ src/textExcel/BaseCell.java
@@ -3,6 +3,12 @@
 
 package textExcel;
 
+/**
+ * An abstract base class for textExcel cells
+ *
+ * Most cells offer the same basic functionality of storing the raw input
+ * and storing it in a private field, then returning it in fullCellText.
+ */
 public abstract class BaseCell implements Cell{
     protected final String rawInput;
     
@@ -10,11 +16,17 @@ public abstract class BaseCell implements Cell{
         this.rawInput = input;
     }
     
+    /**
+     * @return text for spreadsheet cell display, must be exactly length 10
+     */
     @Override
     public String fullCellText() {
         return rawInput;
     }
     
+    /**
+     * @return text for individual cell inspection, not truncated or padded
+     */
     @Override
     public String abbreviatedCellText() {
         return (rawInput + "          ").substring(0, 10);
